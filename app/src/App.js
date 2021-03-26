2a1d6d78490bb055d1cfe348861a4894f696e20d
Ceddyp19 <ceddyp117@gmail.com>
2021-03-25 23:57:47 -0500

    created three additional components: LengthCOntrol, Timer, TimerControl

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
+import TimerControl from './components/TimerControl';
+import React from 'react';
+ 
 
-function App() {
+
+
+class App extends React.Component {
+  render(){
   return (
     <div id='container'>
       <div id="App">
         <div id='title'>25 + 5 Clock</div>
+        <TimerControl />
       </div>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ app/src/components/TimerControl.js
@@ -0,0 +1,6 @@
+
+export default function TimerControl() {
+    return (
+        <h1>Bow Hello!</h1>
+    )
+}
