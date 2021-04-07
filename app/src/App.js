175ee3ed83d14f9d0edfb813e13d78ae28de8b6b
Ceddyp19 <ceddyp117@gmail.com>
2021-04-07 02:25:38 -0500

    made functions to try and get start/stop timing mechanism to work

--- app/src/App.js
+++ app/src/App.js
@@ -11,16 +11,39 @@ class App extends React.Component {
     this.state = {
       breakTime: 5,
       sessionTime: 25,
-      time: '25:00'
+      time: '25:00',
+      isTimerRunning: false
     }
   }
 
   reset = () => {
     console.log('reset time')
+    this.setState({
+      breakTime: 5,
+      sessionTime: 25,
+      time: '25:00'
+    })
   }
 
   toggleTimer = () => {
     console.log('start stop timer')
+
+    this.setState({ isTimerRunning: !this.state.isTimerRunning })
+
+    this.state.isTimerRunning === true ? this.runTimer() : this.pauseTimer();
+
+
+  }
+  //helper function to toggleTimer that runs Timer
+  runTimer = () => {
+    // this.state.isTimerRunning === true ? setInterval(function() {console.log('subtract second'); }, 1000) : clearInterval();
+    // console.log('subtract second')
+ 
+   setInterval(function () { console.log('subtract second'); }, 1000)
+  }
+
+  pauseTimer = () => {
+    clearInterval();
   }
 
   decrementLengthTime = (type) => {
@@ -29,12 +52,14 @@ class App extends React.Component {
       console.log('decrement', this.state[type])
       this.setState({ [type]: this.state[type] - 1 })
     }
-  
+
   }
 
   incrementLengthTime = (type) => {
-    console.log('increment')
-    this.setState({ [type]: this.state[type] + 1 })
+    if (this.state[type] < 60) {
+      console.log('increment')
+      this.setState({ [type]: this.state[type] + 1 })
+    }
   }
 
   render() {
@@ -42,8 +67,8 @@ class App extends React.Component {
       <div id='container'>
         <div id="App">
           <div id='title'>25 + 5 Clock</div>
-          <LengthControl id='break-label' title='Break Length' decrementBtnId='break-decrement' incrementBtnId='break-increment' lengthId='break-length' value={this.state.breakTime} incrementLengthTime={this.incrementLengthTime} decrementLengthTime={this.decrementLengthTime} type='breakTime'/>
-          <LengthControl id='session-label' title='Session Length' decrementBtnId='session-decrement' incrementBtnId='session-increment' lengthId='session-length' value={this.state.sessionTime} incrementLengthTime={this.incrementLengthTime} decrementLengthTime={this.decrementLengthTime} type='sessionTime'/>
+          <LengthControl id='break-label' title='Break Length' decrementBtnId='break-decrement' incrementBtnId='break-increment' lengthId='break-length' value={this.state.breakTime} incrementLengthTime={this.incrementLengthTime} decrementLengthTime={this.decrementLengthTime} type='breakTime' />
+          <LengthControl id='session-label' title='Session Length' decrementBtnId='session-decrement' incrementBtnId='session-increment' lengthId='session-length' value={this.state.sessionTime} incrementLengthTime={this.incrementLengthTime} decrementLengthTime={this.decrementLengthTime} type='sessionTime' />
           <Timer time={this.state.time} />
           <TimerControl reset={this.reset} toggleTimer={this.toggleTimer} />
         </div>
--- app/src/components/LengthControl.js
+++ app/src/components/LengthControl.js
@@ -2,9 +2,9 @@ function LengthControl(props) {
     return (
         <>
             <div id={props.id}>{props.title}</div>
-            <button onClick={() => props.decrementLengthTime(props.type)} className='length-btn' id={props.decrementBtnId}></button>
+            <button onClick={() => props.decrementLengthTime(props.type)} className='length-btn' id={props.decrementBtnId}>decrement</button>
             <div className='length-btn' id={props.lengthId}>{props.value}</div>
-            <button onClick={() => props.incrementLengthTime(props.type)} className='length-btn' id={props.incrementBtnId}></button>
+            <button onClick={() => props.incrementLengthTime(props.type)} className='length-btn' id={props.incrementBtnId}>increment</button>
         </>
     )
 }
