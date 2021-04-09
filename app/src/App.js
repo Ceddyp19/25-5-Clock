5a36cb29db33f1ba18934b18474d2e0025a8abae
Ceddyp19 <ceddyp117@gmail.com>
2021-04-09 17:50:24 -0500

    clock starts but does not stop

--- app/src/App.js
+++ app/src/App.js
@@ -26,24 +26,91 @@ class App extends React.Component {
   }
 
   toggleTimer = () => {
-    console.log('start stop timer')
-
+    // console.log('start stop timer')
     this.setState({ isTimerRunning: !this.state.isTimerRunning })
 
-    this.state.isTimerRunning === true ? this.runTimer() : this.pauseTimer();
-
+    // this.state.isTimerRunning === true ? this.pauseTimer() : this.runTimer();
+    // this.runTimer(time);
+    if (this.state.isTimerRunning) {
+      clearInterval(interval)
+    } else {
+      var interval = setInterval(() => this.runTimer(), 1000);
+    }
 
   }
   //helper function to toggleTimer that runs Timer
   runTimer = () => {
+    let time = this.state.time,
+      minutes = parseInt(time.split(':')[0]),
+      seconds = parseInt(time.split(':')[1]),
+      updatedMinutes,
+      updatedSeconds,
+      updatedTime;
+
+
+    if (minutes === 0 && seconds === 0) return '00:00';
+
+
+    if (seconds > 0) {
+      seconds -= 1
+
+      //when using parseInt, leading zeros are removed.
+      //to replace leading zeros we add '0' at the beginning
+      //which converts the entire statement back into a string
+
+      if (seconds < 10 && minutes < 10) {
+
+        updatedSeconds = '0' + seconds;
+        updatedMinutes = '0' + minutes;
+
+      } else if (seconds < 10 && minutes > 10) {
+
+        updatedSeconds = '0' + seconds;
+        updatedMinutes = minutes;
+
+      } else if (seconds < 10 && minutes > 10) {
+
+        updatedSeconds = seconds;
+        updatedMinutes = '0' + minutes;
+
+      } else if (seconds > 10 && minutes > 10) {
+
+        updatedSeconds = seconds;
+        updatedMinutes = minutes;
+
+      }
+
+    } else if (seconds === 0 && minutes > 0) {
+      seconds = 59
+      minutes -= 1
+
+      if (minutes < 10) {
+        updatedMinutes = '0' + String(minutes);
+        updatedSeconds = seconds;
+      } else {
+        updatedMinutes = minutes;
+        updatedSeconds = seconds;
+      }
+    }
+
+    updatedTime = updatedMinutes + ':' + updatedSeconds
+    console.log(updatedTime)
+    this.setState({ time: updatedTime })
+
+
+
+
+
     // this.state.isTimerRunning === true ? setInterval(function() {console.log('subtract second'); }, 1000) : clearInterval();
     // console.log('subtract second')
- 
-   setInterval(function () { console.log('subtract second'); }, 1000)
+
+    // this.interval = setInterval(function () { console.log('subtract second'); }, 1000)
+    // console.log('bow hello!')
+
   }
 
   pauseTimer = () => {
-    clearInterval();
+    // clearInterval(timer);
   }
 
   decrementLengthTime = (type) => {
